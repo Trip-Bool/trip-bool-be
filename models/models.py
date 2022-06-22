0baaf7f452fe7cde13b3f9e36b7b9f04105e4399
James Brooks <brooksjamesk01@gmail.com>
2022-06-22 16:54:07 -0700

    altered schema to store better values, and alter the string representation to match

--- models/models.py
+++ models/models.py
@@ -6,9 +6,9 @@ class TripModel(db.Model):
     __tablename__ = "trips"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    date_start = db.Column(db.DateTime())
-    date_end = db.Column(db.DateTime())
+    start_date = db.Column(db.Float())
+    end_date = db.Column(db.Float())
     location = db.Column(db.String(80))
     
     def __repr__(self):
-        return f"Name:{self.name}, Date Start:{self.date_start}, Date End: {self.date_end}, Location:{self.location}"
+        return f"Name:{self.name}, Date Start:{self.start_date}, Date End: {self.end_date}, Location:{self.location}"
